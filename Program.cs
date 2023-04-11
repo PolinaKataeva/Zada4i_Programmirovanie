e5b6d15d8e5d9571e45f19ff6359dcdef975cef5
Polina <kataeva.polina@gmail.com>
2023-04-11 21:45:06 +0400

    first commit

---
+++ Program.cs
@@ -0,0 +1,19 @@
+﻿// Задайте значение N. Напишите программу, которая выведет все натуральные числа 
+// в промежутке от N до 1. Выполнить с помощью рекурсии
+
+Console.WriteLine("Введите число N");
+int N = 
+Convert.ToInt32(Console.ReadLine());
+int index = 1;
+while (N >= index)
+{
+    if (N == 1)
+        Console.WriteLine(N);
+        else
+        {
+            return (N-1);
+            Console.WriteLine(N);
+        }
+}
+
+
